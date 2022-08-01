ce88e97f760a3089c5fa2dfbe2e8b54b06584261
saki <sakihet@gmail.com>
2022-08-01 22:09:25 +0900

    update About style

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,10 +3,10 @@
 
 <template>
   <div class="layout-center px-4 py-4">
-    <h1>About</h1>
+    <h1 class="py-6">About</h1>
     <p>Papyls is an experimental note-taking app.</p>
-    <h2>Keywords</h2>
-    <ul>
+    <h2 class="py-6">Keywords</h2>
+    <ul class="text-medium my-0">
       <li>Local-first software</li>
       <li>PWA</li>
     </ul>
