c691a5be36bf212b511bab65235e9e916b2273fa
saki <sakihet@gmail.com>
2022-08-02 22:01:58 +0900

    update TheFooter

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -4,8 +4,9 @@
 <template>
   <div>
     <hr class="border-solid border-t-1 border-color-default" />
-    <div class="flex-row h-4">
-      <div class="f-1 text-center">
+    <div class="flex-row h-6">
+      <div class="f-1 text-center text-small">
+        <a class="p-1 text-decoration-none" href="https://github.com/sakihet/papyls">Code</a>
       </div>
     </div>
   </div>
