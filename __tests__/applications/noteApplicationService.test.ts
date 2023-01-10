046ceab3d376cc912fddd5fdc108c43921e3b04d
saki <sakihet@gmail.com>
2023-01-10 22:49:37 +0900

    add unit tests

---
+++ __tests__/applications/noteApplicationService.test.ts
@@ -0,0 +1,70 @@
+import { v4 } from 'uuid'
+import { beforeAll, describe, expect, it } from 'vitest'
+import { NoteApplicationService } from '../../src/applications/noteApplicationService'
+import { Note } from '../../src/entities/note'
+
+class ArrayRepository {
+  constructor (
+    ary
+  ) {
+    this.ary = ary
+  }
+
+  async add (note: Note) {
+    this.ary.push(note)
+    return note.id
+  }
+
+  async clear () {
+    this.ary = []
+    return
+  }
+
+  async delete (id: string) {
+    const index = this.ary.findIndex(x => x.id === id)
+    this.ary.splice(index, 1)
+    return
+  }
+
+  async get (id: string) {
+    return this.ary.find(x => x.id === id)
+  }
+
+  async getAll () {
+    return this.ary
+  }
+
+  async put (note: Note) {
+    const index = this.ary.findIndex(x => x.id === id)
+    this.ary[index] = note
+    return this.ary[index].id
+  }
+}
+
+class NoteRepository extends ArrayRepository {}
+
+describe('NoteApplicationService', () => {
+  let ary
+  let service
+  beforeAll(() => {
+    ary = []
+    service = new NoteApplicationService(
+      new NoteRepository(ary)
+    )
+  })
+  it('add', async () => {
+    const id = v4()
+    const date = new Date()
+    const n: Note = {
+      id: id,
+      title: 'title',
+      content: 'content',
+      isPinned: false,
+      noteType: 'plain',
+      createdAt: date,
+      updatedAt: date
+    }
+    await service.add(n)
+    expect(ary.length).toEqual(1)
+  })
+})
