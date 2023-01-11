0295d25c3f57bc7539bd40faa424f7747c7ca046
saki <sakihet@gmail.com>
2023-01-12 00:23:09 +0900

    update About

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,11 +5,9 @@ import { name } from '../../package.json'
 <template>
   <div class="layout-center px-4 py-4">
     <h1 class="py-6">About</h1>
-    <p>{{ name }} is an experimental note-taking app.</p>
-    <h2 class="py-6">Keywords</h2>
-    <ul class="text-medium my-0">
-      <li>Local-first software</li>
-      <li>PWA</li>
-    </ul>
+    <div class="text-medium">
+      <p>Paperie is a free, open source note-taking app for introverts.</p>
+      <p>Your notes data is only stored on your browser's storage.</p>
+    </div>
   </div>
 </template>
