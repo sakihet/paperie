391bde97d58907438abb5e312200a55850ed03c9
saki <sakihet@gmail.com>
2023-01-16 21:46:15 +0900

    update tests

--- __tests__/applications/noteApplicationService.test.ts
+++ __tests__/applications/noteApplicationService.test.ts
@@ -1,42 +1,37 @@
 import { v4 } from 'uuid'
-import { beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import { NoteApplicationService } from '../../src/applications/noteApplicationService'
 import { Note } from '../../src/entities/note'
 import { INoteRepository } from '../../src/repositories/noteRepository'
 
 class ArrayNoteRepository implements INoteRepository{
+  ary: Note[]
   constructor (
     ary
   ) {
     this.ary = ary
   }
-
   async add (note: Note) {
     this.ary.push(note)
     return note.id
   }
-
   async clear () {
     this.ary = []
     return
   }
-
   async delete (id: string) {
     const index = this.ary.findIndex(x => x.id === id)
     this.ary.splice(index, 1)
     return
   }
-
   async get (id: string) {
     return this.ary.find(x => x.id === id)
   }
-
   async getAll () {
     return this.ary
   }
-
   async put (note: Note) {
-    const index = this.ary.findIndex(x => x.id === id)
+    const index = this.ary.findIndex(x => x.id === note.id)
     this.ary[index] = note
     return this.ary[index].id
   }
@@ -98,6 +93,18 @@ describe('NoteApplicationService', () => {
     const result = await service.getAll()
     expect(result.length).toEqual(1)
   })
-  it.skip('put', async () => {
+  it('put', async () => {
+    const date = new Date()
+    const newNote: Note = {
+      id: id1,
+      title: 'updated',
+      content: 'content',
+      isPinned: false,
+      noteType: 'plain',
+      createdAt: date,
+      updatedAt: date
+    }
+    const result = await service.put(newNote)
+    expect(result.title).toEqual('updated')
   })
 })
