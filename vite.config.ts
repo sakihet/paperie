1f604b73803ad78d126745e7648da8c022d89b00
saki <sakihet@gmail.com>
2023-01-17 21:42:24 +0900

    add PWA config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,28 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    VitePWA({ registerType: 'autoUpdate'}),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Paperie',
+        short_name: 'Paperie',
+        description: '',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
     vue()
   ]
 })
