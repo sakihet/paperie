d1f7e05a885a43fc6da78b6904eb285cd2e8d5e4
saki <sakihet@gmail.com>
2023-01-27 00:24:13 +0900

    update About

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,8 +5,9 @@
   <div class="layout-center px-4 py-4">
     <h1 class="py-6">About</h1>
     <div class="text-medium">
-      <p>Paperie is a free, open source note-taking app for introverts.</p>
-      <p>Your notes data is only stored on your browser's storage.</p>
+      <p class="my-2">Paperie is a <a href="https://www.inkandswitch.com/local-first/" target="_blank">local-first</a> oriented note-taking app for individuals build with PWA.</p>
+      <p class="my-2">You don't need to create any account to start using it. Just open the url.</p>
+      <p class="my-2">Your notes data is only stored on your browser's IndexedDB storage, so there is no risk of unexpected information leakage. <a href="https://github.com/sakihet/paperie" target="_blank">The code</a> is open source and implementation can be verified.</p>
     </div>
   </div>
 </template>
