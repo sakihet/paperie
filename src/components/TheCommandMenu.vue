c57d72e6fa339383256b39655adf04213dbb2e42
saki <sakihet@gmail.com>
2023-03-06 00:31:54 +0900

    update TheCommandMenu style

--- src/components/TheCommandMenu.vue
+++ src/components/TheCommandMenu.vue
@@ -1,15 +1,35 @@
 <script setup lang="ts">
-import { onUpdated, ref } from 'vue'
+import { onUpdated, ref, nextTick } from 'vue'
 import { store } from '../store'
 import AppCode from './AppCode.vue'
 
 const refCommandMenuInput = ref<HTMLElement | null>(null)
+const refCommandMenuQuery = ref<string>("")
 
 onUpdated(() => {
   if (store.commandMenuDialogOpen) {
     refCommandMenuInput.value?.focus()
   }
 })
+
+const handleKeydown = (e: KeyboardEvent) => {
+  const ignoreKeys = ['Enter', 'ArrowUp', 'ArrowDown']
+  if (ignoreKeys.includes(e.key)) {
+    console.log('ignore', e.key)
+  } else {
+    console.log('keydown', e, refCommandMenuQuery.value)
+  }
+}
+const handleKeydownEnter = (e: Event) => {
+  console.log('keydown enter')
+}
+const handleKeydownUp = (e: Event) => {
+  console.log('keydown up')
+}
+const handleKeydownDown = (e: Event) => {
+  console.log('keydown down')
+}
+
 </script>
 
 <template>
@@ -23,62 +43,64 @@ onUpdated(() => {
       class="border-none shadow rounded"
       :open="store.commandMenuDialogOpen"
     >
-      <div class="layout-stack-4">
+      <div class="">
         <div class="h-8">
           <input
             class="h-8 w-full border-solid border-1 border-color-default rounded px-2"
             type="text"
             placeholder="Under constrcution..."
             ref="refCommandMenuInput"
+            v-model="refCommandMenuQuery"
+            @keydown="handleKeydown"
+            @keydown.enter="handleKeydownEnter"
+            @keydown.up="handleKeydownUp"
+            @keydown.down="handleKeydownDown"
           />
         </div>
         <div>
           <ul class="list-style-none pl-0 my-2">
             <li
-              class="h-8"
+              class="h-8 hover"
               v-for="note in store.notes"
               :key="note.id"
             >
               <router-link
                 class="text-decoration-none"
                 :to="{ path: '/', query: { noteId: note.id} }"
+                @click="store.commandMenuDialogOpen = false"
               >
-                {{ note.title }}
+                <div class="h-8 px-2 py-2">
+                  {{ note.title }}
+                </div>
               </router-link>
             </li>
           </ul>
           <ul class="list-style-none pl-0 my-2">
-            <li
-              class="h-8 hover"
-            >
+            <li class="h-8 hover">
               <router-link
                 class="text-decoration-none"
                 :to="{ path: '/' }"
                 @click="store.commandMenuDialogOpen = false"
               >
-                <div class="h-8 p-2">/</div>
+                <div class="h-8 px-2 py-2">/</div>
               </router-link>
             </li>
-            <li
-              class="h-8 hover"
-            >
+            <li class="h-8 hover">
               <router-link
                 class="text-decoration-none"
                 :to="{ path: '/new' }"
                 @click="store.commandMenuDialogOpen = false"
               >
-               <div class="h-8 p-2">/new</div>
+               <div class="h-8 px-2 py-2">/new</div>
               </router-link>
             </li>
-            <li
-              class="h-8 hover"
-            >
+            <li class="h-8 hover">
               <router-link
                 class="text-decoration-none"
                 :to="{ path: '/about' }"
                 @click="store.commandMenuDialogOpen = false"
               >
-                <div class="h-8 p-2">About</div>
+                <div class="h-8 px-2 py-2">About</div>
               </router-link>
             </li>
           </ul>
