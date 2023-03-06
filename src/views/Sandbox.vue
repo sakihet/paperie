6e4a5031d4f01d8371be526460d2ce8a684dd487
saki <sakihet@gmail.com>
2023-03-06 23:29:37 +0900

    update Sandbox

--- src/views/Sandbox.vue
+++ src/views/Sandbox.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import IconGrid from '../components/IconGrid.vue'
-import IconList from '../components/IconList.vue'
-
 </script>
 
 <template>
@@ -9,18 +6,6 @@ import IconList from '../components/IconList.vue'
     <div class="layout-center">
       <h2>Sandbox</h2>
       <div class="my-4 flex-row">
-        <div class="border-1 border-solid border-button-default divide-solid divide-x rounded">
-          <button class="px-4 py-1 text-small border-none bg-transparent hover">Grid</button>
-          <button class="px-4 py-1 text-small border-none bg-transparent hover">Layout</button>
-        </div>
-      </div>
-      <div class="">
-        <button class="border-none bg-transparent py-0 px-1 hover-icon cursor-pointer rounded h-6">
-          <IconGrid class="text-secondary" />
-        </button>
-        <button class="border-none bg-transparent py-0 px-1 hover-icon cursor-pointer rounded h-6">
-          <IconList class="text-secondary" />
-        </button>
       </div>
     </div>
   </div>
