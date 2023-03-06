6cd5457e7ae21e2e3ac6860b1a2dfab6a16bdc30
saki <sakihet@gmail.com>
2023-03-06 23:59:16 +0900

    update NoteItem layout

--- src/components/NoteItem.vue
+++ src/components/NoteItem.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { Note } from '../entities/note'
 import { formatDate } from '../utils'
 import AppTextarea from '../components/AppTextarea.vue'
 import NoteDropdown from './NoteDropdown.vue'
+import { store } from '../store'
 
 const props = defineProps<{
   layout: string,
@@ -11,6 +12,13 @@ const props = defineProps<{
   selectedNoteId: string | undefined
 }>()
 const isHovered = ref(false)
+const classObject = computed(() => ({
+  'w-48 h-48': store.notesLayout === 'grid',
+  'h-24': store.notesLayout === 'list',
+  'border-t-2 border-color-blue border-solid': props.note.isPinned && store.notesLayout === 'grid',
+  'border-l-2 border-color-blue border-solid': props.note.isPinned && store.notesLayout === 'list',
+  'bg-selected': props.note.id === props.selectedNoteId
+}))
 </script>
 
 <template>
@@ -22,46 +30,7 @@ const isHovered = ref(false)
       @mouseover="isHovered = true"
       @mouseleave="isHovered = false"
     >
-      <div
-        v-if="props.layout === 'grid'"
-        class="flex-column"
-        :class="{ 'border-t-2 border-color-blue border-solid': note.isPinned, 'w-48': true, 'h-48' : true }"
-      >
-        <div class="p-2 layout-stack-1">
-          <div class="h-6 flex-row">
-            <div class="f-1 font-semibold overflow-hidden text-overflow-ellipsis">
-              <span class="nowrap">{{ note.title }}</span>
-            </div>
-            <div class="w-6">
-              <div v-if="isHovered">
-                <NoteDropdown :note="note"/>
-              </div>
-            </div>
-          </div>
-          <div class="f-1">
-            <AppTextarea
-              class="text-secondary bg-primary"
-              :id="note.id"
-              :content="note.content"
-              :rows="8"
-              :cols="16"
-            />
-          </div>
-          <div class="h-4 flex-row">
-            <div class="text-small text-secondary f-1">
-              {{ formatDate(note.updatedAt) }}
-            </div>
-            <div class="text-small text-secondary text-right border-1 border-solid border-color-default px-1">
-              {{ note.noteType }}
-            </div>
-          </div>
-        </div>
-      </div>
-      <div
-        v-else-if="props.layout === 'list'"
-        class="h-24"
-        :class="{ 'border-l-2 border-color-blue border-solid': note.isPinned, 'bg-selected': note.id === selectedNoteId, 'bg-primary': note.id !== selectedNoteId }"
-      >
+      <div :class="classObject">
         <div class="p-2 layout-stack-1">
           <div class="overflow-hidden h-6 flex-row">
             <div class="f-1 font-semibold overflow-hidden text-overflow-ellipsis">
@@ -73,7 +42,22 @@ const isHovered = ref(false)
               </div>
             </div>
           </div>
-          <div class="h-8 overflow-hidden text-secondary">
+          <div
+            v-if="props.layout === 'grid'"
+            class="f-1"
+          >
+            <AppTextarea
+              class="text-secondary bg-primary"
+              :id="note.id"
+              :content="note.content"
+              :rows="8"
+              :cols="16"
+            />
+          </div>
+          <div
+            v-else-if="props.layout === 'list'"
+            class="h-8 overflow-hidden text-secondary"
+          >
             <div class="text-small">{{ note.content }}</div>
           </div>
           <div class="h-4 flex-row">
