292f7281ab749c3cd57cff3d44a0fc96690e7cc7
saki <sakihet@gmail.com>
2023-03-16 22:12:08 +0900

    fix layout

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
-import AppCode from '../components/AppCode.vue'
 </script>
 
 <template>
-  <div class="layout-center px-4 py-4 f-1">
-    <h1 class="my-8">About</h1>
-    <div class="text-medium">
-      <p class="my-2">Paperie is a <a href="https://www.inkandswitch.com/local-first/" target="_blank">local-first</a> oriented note-taking app for individuals build with PWA.</p>
-      <p class="my-2">You don't need to create any account to start using it. Just open the url.</p>
-      <p class="my-2">Your notes data is only stored on your browser's IndexedDB storage, so there is no risk of unexpected information leakage. <a href="https://github.com/sakihet/paperie" target="_blank">The code</a> is open source and implementation can be verified.</p>
+  <div class="p-4 f-1">
+    <div class="layout-center">
+      <h1 class="my-8">About</h1>
+      <div class="text-medium">
+        <p class="my-2">Paperie is a <a href="https://www.inkandswitch.com/local-first/" target="_blank">local-first</a> oriented note-taking app for individuals build with PWA.</p>
+        <p class="my-2">You don't need to create any account to start using it. Just open the url.</p>
+        <p class="my-2">Your notes data is only stored on your browser's IndexedDB storage, so there is no risk of unexpected information leakage. <a href="https://github.com/sakihet/paperie" target="_blank">The code</a> is open source and implementation can be verified.</p>
+      </div>
     </div>
-</div>
+  </div>
 </template>
