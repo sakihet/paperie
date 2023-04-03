4a682644ce1bef64430d74b37d419715b6198e5c
saki <sakihet@gmail.com>
2023-04-03 21:37:21 +0900

    add created to TheNoteEditorHeader

--- src/components/TheNoteEditorHeader.vue
+++ src/components/TheNoteEditorHeader.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import { Ref, ref } from 'vue'
 import { currentNote, store } from '../store'
 import { NoteType } from '../types/noteType'
 import { formatDate } from '../utils'
 
-const handleClick = (e: Event, noteType: NoteType) => {
+type dateType = 'updated' | 'created'
+
+const dateType: Ref<dateType> = ref('updated')
+
+const handleClickDate = (e: Event) => {
+  if (dateType.value === 'updated') {
+    dateType.value = 'created'
+  } else {
+    dateType.value = 'updated'
+  }
+}
+const handleClickNoteType = (e: Event, noteType: NoteType) => {
   store.actions.note.updateNoteType(store, noteType)
 }
 </script>
@@ -14,20 +26,28 @@ const handleClick = (e: Event, noteType: NoteType) => {
       <button
         class="h-6 border-none text-secondary hover px-2 cursor-pointer"
         :class="store.editor.noteType === 'plain' ? 'bg-selected-text' : 'bg-transparent'"
-        @click="handleClick($event, 'plain')"
+        @click="handleClickNoteType($event, 'plain')"
       >Plain</button>
       <button
         class="h-6 border-none text-secondary hover px-2 cursor-pointer"
         :class="store.editor.noteType === 'markdown' ? 'bg-selected-text' : 'bg-transparent'"
-        @click="handleClick($event, 'markdown')"
+        @click="handleClickNoteType($event, 'markdown')"
       >Markdown</button>
     </div>
     <div class="nowrap">
       <div class="h-6 flex-row">
         <div class="m-auto">
           <span
-            class="text-small text-secondary text-overflow-ellipsis"
-            v-if="currentNote?.updatedAt"
+            class="text-small text-secondary text-overflow-ellipsis cursor-pointer user-select-none"
+            v-if="currentNote?.createdAt && dateType === 'created'"
+            @click="handleClickDate"
+          >
+            Created: {{ formatDate(currentNote?.createdAt!) }}
+          </span>
+          <span
+            class="text-small text-secondary text-overflow-ellipsis cursor-pointer user-select-none"
+            v-if="currentNote?.updatedAt && dateType === 'updated'"
+            @click="handleClickDate"
           >
             Updated: {{ formatDate(currentNote?.updatedAt!) }}
           </span>
